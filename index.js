abe5d7e0fe0ae0d29b4ffffa8d0f4ad7c93d4a4a
Kurt Baux <ktripodibaux@gmail.com>
2022-06-06 12:11:20 -0400

    commit tree e6b405032e181130e25da70d7714330b8f7d852d

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
   // Write your code here!
+  if (depth <= 400) {
+    return 'This one is on me!';
+  } 
+  else if (depth <= 2000) {
+    return 'That will be twenty bucks.';
+  } 
+  else if (depth <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  } 
+  else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city == 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip == 'generous') {
+    return 'Thank you so much.';
+  }
+  else if (tip == 'not as generous') {
+    return 'Thank you.'
+  }
+  else {
+    return 'Bye.';
+  }
 }
